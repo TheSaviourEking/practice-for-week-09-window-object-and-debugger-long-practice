c3b147fe797b2fe2e1f724bc3714af42da37b17c
TheSaviourEking <saviour.eking@outlook.com>
2024-02-02 02:21:30 +0100

    [chore]: implement the phases 1 - 3 requirements of the project

--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,18 +1,25 @@
-function changeTitle() {
+const name = 'Saviour Eking'
+export function changeTitle() {
     // Change the title of the page to "(Your name)'s Portfolio"
 
     // Your code here
+    const title = `${name}'s Portfolio`;
+    document.title = title;
 }
 
-function changeHeader() {
+export function changeHeader() {
     // Change the name in the h1 of the page to your name
 
     // Your code here
+    document.body.children[0].children[0].innerText = `I am ${name}`;
 }
 
-function changeAboutMe() {
+export function changeAboutMe() {
     /* Update the first paragraph in the About Me section with a small
      passage about yourself */
 
     // Your code here
+    const text = 'Hello there! I\'m passionate about technology and love creating innovative solutions. My journey in the tech world started with a curiosity for coding, and it has been an exciting adventure ever since. I enjoy tackling challenges and constantly expanding my skills to stay on the cutting edge of technology.'
+    const paragraph = document.body.children[1].children[1];
+    paragraph.innerText = text;
 }
